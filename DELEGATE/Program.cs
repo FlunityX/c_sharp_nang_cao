02584ef4e0bf06006995a0075a894a0cd0396484
FlunityX <149858650+FlunityX@users.noreply.github.com>
2023-12-07 10:55:03 +0700

    delegate

---
+++ DELEGATE/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace delegate1{
+
+    internal class Program
+    {
+        public delegate void ShowLog(string message);
+        static void info (string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+        static void Main(string[] args)
+        {
+            ShowLog log;
+            log = info;
+            log("xin chao ");
+
+        }
+    } 
+}
