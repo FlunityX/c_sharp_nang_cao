f784667e6ca89bba34eefbd7d98fa532dc6b2667
FlunityX <149858650+FlunityX@users.noreply.github.com>
2023-12-08 23:53:36 +0700

    event trong c#

---
+++ event/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace event1;
+
+internal class Program
+{
+    // Một Publisher trong C# là một đối tượng mà chứa định nghĩa của event và delegate đó. Mối liên hệ event-delegate cũng được định nghĩa trong đối tượng này.
+    // Một đối tượng lớp Publisher triệu hồi Event và nó được thông báo tới các đối tượng khác.
+    //Một Subscriber trong C# là một đối tượng mà chấp nhận event và cung cấp một Event Handler
+    //. Delegate trong lớp Publisher triệu hồi phương thức (Event Handler) của lớp Subscriber
+    public class EventManager
+    {
+        public delegate void EventHandler(string message);
+        public event EventHandler EventOccurred;
+
+        public void TriggerEvent(string message)
+        {
+            // Kiểm tra xem sự kiện có người đăng ký hay không trước khi gọi
+            EventOccurred?.Invoke(message);
+        }
+    }
+
+    public class Subscriber
+    {
+        private string name;
+
+        public Subscriber(string name, EventManager eventManager)
+        {
+            this.name = name;
+            eventManager.EventOccurred += HandleEvent;
+        }
+
+        public void HandleEvent(string message)
+        {
+            Console.WriteLine($"{name} received event: {message}");
+        }
+    }
+        static void Main(string[] args)
+        {
+            EventManager eventManager = new EventManager();
+            Subscriber subscriber1 = new Subscriber("Subscriber 1", eventManager);
+            Subscriber subscriber2 = new Subscriber("Subscriber 2", eventManager);
+
+            eventManager.TriggerEvent("Hello, World!");
+
+            Console.ReadLine();
+        }
+
+}
---
+++ áp_dung_event/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+
+namespace áp_dung_event
+{
+    internal class Program
+    {
+        public class nhanvat
+        {
+            public delegate void chuyendong(string a);
+            public event chuyendong chuyendonghientai;
+            public void kiemtrachuyendong(string a)
+            {
+                chuyendonghientai?.Invoke(a);
+            }
+            public void kiemtrachuyendong1(string a)
+            {
+                chuyendonghientai?.Invoke(a);
+            }
+
+        }
+        public class nguoichoi 
+        {
+            int so ;
+            public nguoichoi(int so, nhanvat nhanvat1)
+            {
+                this.so = so;
+                nhanvat1.chuyendonghientai += dachuyendong;
+               
+                
+            }
+            public void dachuyendong(string a)
+            {
+                Console.WriteLine("nhan vat {0} da chuyen dong , dang hang dong la :{1} ",so,a);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // tao 1 doi tg cho lop nhan vat - pub 
+            // tao 1 doi tg lop nguoi choi - sub 
+            nhanvat nv = new nhanvat();
+            // truyen vao nv 
+            nguoichoi nc1 = new nguoichoi(1, nv);
+            // kiem tra loai event 
+            nv.kiemtrachuyendong("nhay"); // suy ra + dachuyendong
+            nv.kiemtrachuyendong1("chay");// suyra + dachuyendong1
+            Console.ReadLine();
+        }
+    }
+}
