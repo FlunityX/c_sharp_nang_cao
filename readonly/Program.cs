39718a83aa7d6cc65b9985543dbeae10759d6585
nguyễn mạnh <manharia123@gmail.com>
2024-02-03 16:14:30 +0700

    biến readonly trong csharp

---
+++ readonly/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace read_only;
+class read_only
+{
+    private readonly int age;
+    public read_only(int age) 
+    {
+        this.age = age;
+    }
+    public void use_age()
+    {
+        Console.WriteLine("tuoi "+age);
+    }
+}
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        read_only read_ = new read_only(4);
+        read_.use_age();
+    }
+}
+
