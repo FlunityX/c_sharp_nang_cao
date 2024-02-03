6c82e50079b5bd44fe9bde327a2a751d1e75bd31
nguyễn mạnh <manharia123@gmail.com>
2024-02-03 16:15:13 +0700

    string builder trong c#

---
+++ string_builder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace string_builder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("hello");
+            stringBuilder.Insert(5, " world");
+                              
+            stringBuilder.Replace("world", "universe");
+            string stringresult = stringBuilder.ToString();
+            Console.Write(stringresult);
+            Console.ReadLine();
+        }
+    }
+}
