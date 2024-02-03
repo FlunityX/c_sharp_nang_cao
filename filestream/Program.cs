736cdecccf6461901e406a9e74a758d020393ba2
nguyễn mạnh <manharia123@gmail.com>
2024-02-03 16:13:13 +0700

    làm tròn số thập phân

---
+++ filestream/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace filestream
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "data.cs";
+            using var stream = new FileStream(path:path,FileMode.Create);
+            // lưu data 
+            byte[] buffer = { 1, 2, 3 };
+            stream.Write(buffer, 0, buffer.Length);
+            // đọc dữ liệu 
+            int sobytedocdc =stream.Read(buffer, 0, buffer.Length);
+            // int,docble --> byte 
+            int abc = 1;
+            var byte_abc = BitConverter.GetBytes(abc);
+            string s = "abc";
+            var byte_s = Encoding.UTF8.GetBytes(s);
+            // byte --> int , double ...
+            BitConverter.ToInt32(byte_abc, 0);
+            Encoding.UTF8.GetString(byte_s, 0, byte_s.Length);
+
+        }
+    }
+}
---
+++ làm_tròn_số/làm_tròn_số/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        float number = 3.7f;
+
+        int ceilResult = (int)Math.Ceiling(number); // ceilResult = 4
+        int floorResult = (int)Math.Floor(number); // floorResult = 3
+        int roundedResult = (int)Math.Round(number); // roundedResult = 4
+
+      
+        Console.WriteLine("so dc ceil :"+ceilResult);
+        Console.WriteLine("so dc floor :"+floorResult);
+        Console.WriteLine("so dc round :"+ roundedResult);
+        Console.ReadLine();
+    }
+}
