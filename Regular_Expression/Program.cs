333a7361f3a442421233c6eeadb6a4d468046b92
nguyễn mạnh <manharia123@gmail.com>
2024-02-05 15:46:33 +0700

    regular expression

---
+++ Regular_Expression/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Regular_Expression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Hello123";
+
+            // Kiểm tra xem chuỗi có chứa chữ số hay không
+            bool isMatch = Regex.IsMatch(input, @"\d");
+
+            Console.WriteLine(isMatch);
+            string input1 = "Hello123 World456";
+
+            // Tìm tất cả các chuỗi chứa chữ số
+            MatchCollection matches = Regex.Matches(input1, @"\d");
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Value); // Output: 1, 2, 3, 4, 5, 6
+            }
+            Console.ReadLine();
+        }
+
+       
+        }
+  
+    }
+
