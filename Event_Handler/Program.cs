6776eeb5c1505e623ef25a69dedafe4be24abe1b
nguyễn mạnh <manharia123@gmail.com>
2024-02-05 15:45:00 +0700

    event Handler c#

---
+++ Event_Handler/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Event_Handler
+{
+    public class myevent : EventArgs
+    {
+        public myevent(string data) 
+        {
+            this.data =data;
+        }
+        // lưu dữ liệu từ publisher
+        private string data;
+        // property
+        public string Getdata(string data)
+        {
+            return data;
+        }
+
+    } 
+    public class A
+    {
+        public event EventHandler<myevent> handler;
+        public void send()
+        {
+           handler?.Invoke(this , new myevent("đã nhận sự kiện"));
+        }
+    }
+    class B 
+    {
+        public void sub(A p)
+        {
+            p.handler += nhansk;
+        }
+        public void nhansk(object sender ,myevent e)
+        {
+            Console.WriteLine("class B"+ e.Getdata("b"));
+        }
+
+    }
+    class c
+    {
+        public void sub(A p)
+        {
+            p.handler += nhansk;
+        }
+        public void nhansk(object sender, myevent e)
+        {
+            Console.WriteLine("class c" + e.Getdata("c"));
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            B b = new B();
+            c c = new c();
+            c.sub(a);
+            b.sub(a);
+            a.send();
+            Console.ReadLine();
+        }
+    }
+}
