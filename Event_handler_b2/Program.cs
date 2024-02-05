20792a26ec8f40759e7c860fc7842b91eb0ada87
nguyễn mạnh <manharia123@gmail.com>
2024-02-05 15:45:37 +0700

    bài tập

---
+++ Event_handler_b2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Event_handler_b2
+{
+    public delegate void myeventhandler (object sender , EventArgs s);
+    public class myevent
+    {
+        public event myeventhandler handler;
+        public void MyEventHandlerMethod(object sender , EventArgs s)
+        {
+            Console.WriteLine("event active");
+        } 
+        public void triggerEvent()
+        {
+            handler?.Invoke(this,EventArgs.Empty);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          myevent myevent = new myevent();
+            myevent.handler += myevent.MyEventHandlerMethod;
+            myevent.triggerEvent();
+        }
+
+       
+    }
+}
