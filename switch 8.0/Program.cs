b07ad74d4d407b9729e00ca460c3e23a232a499a
nguyễn mạnh <manharia123@gmail.com>
2024-02-14 18:16:29 +0700

    switch c# 8.0

---
+++ switch 8.0/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Switch_8_0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* expression switch
+            {
+                pattern1 => Mã khi pattern1 khớp với expression,
+                pattern2 =>  Mã khi pattern2 khớp với expression,
+                            Các mẫu khác...
+                _ =>  Mã mặc định khi không có mẫu nào khớp với expression
+            };*/
+            char grade = 'A';
+            string result = grade switch
+            {
+                'A' => "excellent",
+                'B' => "good",
+                _=> "needs improvement"
+            };
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
