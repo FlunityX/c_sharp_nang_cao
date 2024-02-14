8d9d9c28ae625978f86f1ed119302bfc9d58121c
nguyễn mạnh <manharia123@gmail.com>
2024-02-14 17:29:16 +0700

    từ khoá nameof

---
+++ nameof/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _name
+{
+    enum color
+    {
+        red,
+        blue,
+        green,
+        yellow
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+            // nameof biến đổi tên biến thành chuỗi 
+        {
+           color a = color.red;
+           string enumstring = nameof(color)+ "." + nameof(color.blue);
+           Console.WriteLine(enumstring);
+           Console.ReadLine();
+        }
+    }
+}
