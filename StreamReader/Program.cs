b45821c2248e62025aba3baebbedbdfae34c57d9
nguyễn mạnh <manharia123@gmail.com>
2024-02-14 17:30:14 +0700

    đọc dữ liệu bằng StreamReader

---
+++ StreamReader/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace StreamReaderExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"D:\design_pattern\singleton\nhanvatgame\Program.cs";
+
+            try
+            {
+                // Open the JavaScript file to read
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    // Read the content of the file
+                    string content = sr.ReadToEnd();
+
+                    // Display the content of the file
+                    Console.WriteLine(content);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
