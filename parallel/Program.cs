12db0b1988f163e43655bb63fc579a1b9a6a7479
nguyễn mạnh <manharia123@gmail.com>
2024-03-17 15:03:54 +0700

    parallel

---
+++ parallel/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace parallel
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+
+            Parallel.ForEach(numbers, (number) =>
+            {
+                Console.WriteLine(number);
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
